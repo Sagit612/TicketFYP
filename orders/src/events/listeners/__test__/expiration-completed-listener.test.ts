039e83d6be532e2fad5bb4910f3ee7c56da7a56a
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-11-05 18:23:57 +0700

    Finished testing Expiration Completed Listener

--- orders/src/events/listeners/__test__/expiration-completed-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-completed-listener.test.ts
@@ -46,12 +46,16 @@ it('updates the order status to cancelled', async () => {
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-// it('emit an OrderCancelled event', async() => {
-//     const {listener, data, msg, newOrder, newTicket} = await setup();
-//     await listener.onMessage(data, msg);
-//     (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
-// });
+it('emit an OrderCancelled event', async() => {
+    const {listener, data, msg, newOrder, newTicket} = await setup();
+    await listener.onMessage(data, msg);
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+    const eventData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
+    expect(eventData.id).toEqual(newOrder.id);
+});
 
-// it('ack the message', async() => {
-
-// })
+it('ack the message', async() => {
+    const {listener, data, msg, newOrder, newTicket} = await setup();
+    await listener.onMessage(data, msg);
+    expect(msg.ack).toHaveBeenCalled();
+})
