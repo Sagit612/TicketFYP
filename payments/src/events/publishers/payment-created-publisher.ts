2c7e3ea6e4e53e3870c0573e5ea8124de24558a4
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-11-06 22:46:01 +0700

    Initial setup for payment created publisher

---
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Subjects, Publisher, PaymentCreatedEvent,  } from "@sagittickets/common";
+
+class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+}
