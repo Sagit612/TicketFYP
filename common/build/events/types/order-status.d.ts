704c4a5096da2b4681ebd14161331e0d501dc084
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-11-13 23:53:19 +0700

    Updates

--- common/build/events/types/order-status.d.ts
+++ common/build/events/types/order-status.d.ts
@@ -2,5 +2,5 @@ export declare enum OrderStatus {
     Created = "created",
     Cancelled = "cancelled",
     AwaitingPayment = "awaiting:payment",
-    Completed = "completed"
+    Complete = "complete"
 }
